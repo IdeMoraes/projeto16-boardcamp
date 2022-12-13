ef7e2b3f27044b6d73a7d51d9c7ce05e6d8c3a74
Igor de Moraes <r.de.moraes.igor@gmail.com>
2022-12-13 16:01:37 -0300

    Feature DELETE Rental

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -46,7 +46,7 @@ export async function finishRental (req, res){
         if (differenceInDays > daysRented) {
             delayFee = (differenceInDays - daysRented) * originalPrice;
         };
-        const result = await db.query(`UPDATE rentals SET "returnDate" = NOW(), "delayFee" = $1 WHERE id = $2`, [delayFee, id]);
+        const result = await connection.query(`UPDATE rentals SET "returnDate" = NOW(), "delayFee" = $1 WHERE id = $2`, [delayFee, id]);
         if (result.rowCount>=1){
             res.sendStatus(200);
         };
@@ -57,7 +57,20 @@ export async function finishRental (req, res){
 };
 
 export async function deleteRental (req, res){
+    const {id} = req.params;
     try {
+        const rental = await connection.query(`SELECT * FROM rentals WHERE id = $1`, [id]);
+        if(rental.rowCount === 0) {
+            return res.sendStatus(404);
+        };
+        const {returnDate} = rental.rows[0];
+        if(!returnDate){
+            return res.sendStatus(400); 
+        };
+        const result = await connection.query(`DELETE FROM rentals WHERE id = $1`, [id]);
+        if (result.rowCount>=1){
+            res.sendStatus(200);
+        };
     } catch (error) {
         console.log(error);
         res.send(`${error.name}: ${error.message}`);
